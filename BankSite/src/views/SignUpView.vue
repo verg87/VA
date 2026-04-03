83e30861901975da98fbdf04324d52fb8c78c0ec
victo <victoryellow300@gmail.com>
2026-04-03 22:49:22 +0500

    Decided that sign up page should look and behave differently. Now it has appearing one after another stages where user needs to fill up the one or two fields at once. Added email field to the sign up

--- BankSite/src/views/SignUpView.vue
+++ BankSite/src/views/SignUpView.vue
@@ -1,9 +1,13 @@
 <script setup>
 import { RouterLink } from "vue-router";
+import { ref, computed } from "vue";
 import router from "@/router";
 import axios from "axios";
 
 import "../assets/auth.css";
+
+const currentActiveSignUpStage = ref("email");
+const signUpData = ref({});
 
 const validateSignUpFormFields = async (event) => {
   let data = {};
@@ -36,45 +40,148 @@ const validateSignUpFormFields = async (event) => {
     alert("Oops something is wrong");
   }
 };
+
+const stages = ref({
+  email: {
+    headerText: "Enter your email",
+    inputType: "text",
+    inputId: "email",
+    hasEntered: false,
+  },
+  phoneNumber: {
+    headerText: "Enter your phone number",
+    inputType: "text",
+    inputId: "phone-number",
+    hasEntered: false,
+  },
+  names: {
+    headerText: "Enter your first and last name",
+    first: {
+      inputType: "text",
+      inputId: "name"
+    },
+    last: {
+      inputType: "text",
+      inputId: "lastname",
+    },
+    hasEntered: false,
+  },
+  password: {
+    headerText: "Create a password",
+    inputType: "password",
+    inputId: "password",
+    hasEntered: false,
+  },
+  passwordConfirmation: {
+    headerText: "Confirm your password",
+    inputType: "password",
+    inputId: "password-confirmation",
+    hasEntered: false,
+  }
+});
+
+const processSignUpStage = () => {
+  const stageName = currentActiveSignUpStage.value;
+  let currentStageInputsValid = true;
+
+  if (stageName === "names") {
+    const firstNameField = document.getElementById(stages.value.names.first.inputId);
+    const lastNameField = document.getElementById(stages.value.names.last.inputId);
+
+    if (!firstNameField.value || !lastNameField.value) {
+      alert("First name and last name shouldn't be empty");
+      currentStageInputsValid = false;
+    } else {
+      signUpData.value = {
+        ...signUpData.value,
+        [firstNameField.id]: firstNameField.value,
+        [lastNameField.id]: lastNameField.value,
+      };
+    }
+  } else {
+    const id = stages.value[stageName].inputId;
+    const field = document.getElementById(id);
+
+    if (!field || !field.value) {
+      alert("Field shouldn't be empty");
+      currentStageInputsValid = false;
+    } else if (
+      id === "password-confirmation" &&
+      field.value !== signUpData.value["password"]
+    ) {
+      alert("Two passwords aren't matching");
+      currentStageInputsValid = false;
+    } else {
+      signUpData.value = {
+        ...signUpData.value,
+        [id]: field.value,
+      };
+    }
+  }
+
+  if (!currentStageInputsValid) {
+    return;
+  }
+  
+  stages.value[stageName].hasEntered = true;
+
+  const newStage = Object.entries(stages.value)
+    .find(([key, value]) => !value.hasEntered);
+
+  if (newStage) {
+    currentActiveSignUpStage.value = newStage[0];
+  } else {
+    console.log("Sign Up process complete");
+  }
+}
+
+const getPreviousBtnVisibility = computed(() => {
+  return currentActiveSignUpStage.value !== "email";
+});
+
+const getNextBtnVisibility = computed(() => {
+  const currentStageKey = currentActiveSignUpStage.value;
+  const stage = stages.value[currentStageKey];
+  const stageOrder = Object.keys(stages.value);
+  const currentIndex = stageOrder.indexOf(currentStageKey);
+
+  // The 'Next' button should be visible until the last stage is processed and ready to submit
+  return currentIndex < stageOrder.length - 1 && stage.hasEntered;
+});
+
+const changeStage = (event) => {
+  const currentStageKey = currentActiveSignUpStage.value;
+  const stageOrder = Object.keys(stages.value);
+  const currentIndex = stageOrder.indexOf(currentStageKey);
+
+  if (currentIndex - 1 >= 0 && event.target.id === "previous") {
+    currentActiveSignUpStage.value = stageOrder[currentIndex - 1];
+  } else if (currentIndex + 1 <= stageOrder.length && event.target.id === "next") {
+    currentActiveSignUpStage.value = stageOrder[currentIndex + 1];
+  }
+}
+
+const getFields = computed(() => {
+  const stage = stages.value[currentActiveSignUpStage.value];
+  const fields = Object.values(stage).filter((prop) => typeof prop === "object");
+
+  return fields.length ? fields : [stage];
+});
 </script>
 
 <template>
-  <p class="header-1">Register Here</p>
-  <div class="panel">
-    <p class="header-2">Sign Up</p>
-    <form
-      @submit.prevent="validateSignUpFormFields"
-      action=""
-      method="post"
-      class="auth"
-    >
-      <div class="field">
-        <label for="name">Enter your name</label>
-        <input type="text" id="name" />
-      </div>
-
-      <div class="field">
-        <label for="lastname">Enter your lastname</label>
-        <input type="text" id="lastname" />
-      </div>
-
-      <div class="field">
-        <label for="phone-number">Enter your phone number</label>
-        <input type="text" id="phone-number" />
-      </div>
-
-      <div class="field">
-        <label for="password">Create a password for your account</label>
-        <input type="password" id="password" />
-      </div>
-
-      <div class="field">
-        <label for="password-confirmation">Confirm your password</label>
-        <input type="password" id="password-confirmation" />
-      </div>
-
-      <button type="submit">Click to register</button>
-    </form>
+  <div style="display: flex; justify-content: space-between; width: fit-content">
+    <button id="previous" v-show="getPreviousBtnVisibility" @click="changeStage">*back image</button>
+    <p class="header-1">{{ stages[currentActiveSignUpStage].headerText }}</p>
+    <button id="next" v-show="getNextBtnVisibility" @click="changeStage">*forward image</button>
+  </div>
+  <div v-for="item in getFields">
+    <div class="field">
+      <input :type=item.inputType :id=item.inputId>
+    </div>
+  </div>
+  <div>
+    <button @click="processSignUpStage">Next</button>
   </div>
   <RouterLink to="/login" class="redirect-btn"
     >Already have an account? Login</RouterLink
